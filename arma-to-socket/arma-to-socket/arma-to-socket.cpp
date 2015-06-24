e8b6b6797385cbf5480932a441638aef1667d2bf
Loenja Selter <LoenjaSelter@gmail.com>
2015-06-24 15:50:13 +0100

    removed redundant projects

--- arma-to-socket/arma-to-socket/arma-to-socket.cpp
+++ arma-to-socket/arma-to-socket/arma-to-socket.cpp
@@ -1,16 +1,16 @@
 // threaded_example.cpp : Defines the exported functions for the DLL application.
-// 
+//
 
-#include "stdafx.h" 
-#include <string> 
-#include <unordered_map> 
-#include <thread> 
-#include <mutex> 
-#include <atomic> 
+#include "stdafx.h"
+#include <string>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 #include <fstream>
 
-#include <boost/program_options.hpp> 
+#include <boost/program_options.hpp>
 
 #include <Poco/Net/StreamSocket.h>
 #include <Poco/Net/SocketAddress.h>
@@ -76,7 +76,7 @@ std::vector<std::string>& split(const std::string &s, char delim, std::vector<st
 std::vector<std::string> split(const std::string &s, char delim);
 std::atomic<bool> worker_working(false);
 long int id = 0; // global ticket id
-long int cur_id = 0;// current ticket id 
+long int cur_id = 0;// current ticket id
 std::string newSocket(std::vector<std::string>&);
 std::string TCPSend(std::vector<std::string>&);
 std::string TCPRecv(std::vector<std::string>&);
@@ -93,12 +93,12 @@ int maxLen;
 extern "C" { __declspec (dllexport) void __stdcall RVExtension(char *output, int outputSize, const char *function); }
 
 void worker() {
-	while (worker_working = id > cur_id) // next ticket exists? 
+	while (worker_working = id > cur_id) // next ticket exists?
 	{
 		mtx.lock();
-		Data ticket = tickets[++cur_id];// copy ticket 
+		Data ticket = tickets[++cur_id];// copy ticket
 		mtx.unlock();
-		string input = ticket.params; // get input 
+		string input = ticket.params; // get input
 		string output = "output: " + input; // process input
 		//Sleep(10); //sleep for 0.01 sec (FOR TESTING PURPOSES ONLY)
 
@@ -133,22 +133,121 @@ void worker() {
 		vect = split(input, *A2S_DELIMINATOR);
 		//std::transform(vect[1].begin(), vect[1].end(), vect[1].begin(), ::tolower);
 		//try {
-			int length = vect.size();
-			//std::cout << length << std::endl;
-			switch (systemCalls[vect[0]]){
-				case 1:
-				{
-					//std::wcout << "version....." << std::endl;
-					output = A2S_VERSION;
-					break;
-				}
-				case 2:
-				{
-					output = A2S_API_VERSION;
-					break;
-				}
+		int length = vect.size();
+		//std::cout << length << std::endl;
+		switch (systemCalls[vect[0]]){
+			case 1:
+			{
+				//std::wcout << "version....." << std::endl;
+				output = A2S_VERSION;
+				break;
+			}
+			case 2:
+			{
+				output = A2S_API_VERSION;
+				break;
+			}
 				//TCP
-				case 3:
+			case 3:
+			{
+				if (length < 3){
+					output = "TCP calls requires at least 3 arguments. You tried: '" + input + "'";
+					break;
+				}
+				else {}
+
+				switch (tcp[vect[1]])
+				{
+					//create new socket
+					case 1:
+					{
+						//std::cout << "creating socket" << std::endl;
+						if (length < 3){
+							output = "create call requires at least 3 arguments. You tried: '" + input + "'";
+							break;
+						}
+						else
+						{
+							output = newSocket(vect);
+						}
+
+						break;
+					}
+						//send
+					case 2:
+					{
+						if (length < 4){
+							output = "send requires at least 4 arguments. You tried: '" + input + "'";
+							break;
+						}
+						else{
+							output = TCPSend(vect);
+						}
+
+
+						break;
+
+					}
+						//recieve
+					case 3:
+					{
+
+						if (length < 3){
+							output = "recieve requires at least 2 arguments. You tried: '" + input + "'";
+							break;
+						}
+						else {
+							output = TCPRecv(vect);
+						}
+
+						break;
+					}
+
+						//send recieve
+					case 4:
+					{
+						//std::cout << "recieving" << std::endl;
+						if (length < 4){
+							output = "recieve requires at least 4 arguments. You tried: '" + input + "'";
+							break;
+						}
+						else {
+							TCPSend(vect);
+							output = TCPRecv(vect);
+						}
+						break;
+					}
+
+						//close
+					case 5:
+					{
+						if (length < 2){
+							output = "recieve requires at least 2 arguments. You tried: '" + input + "'";
+							break;
+						}
+
+
+						break;
+					}
+
+					default:
+					{
+						break;
+					}
+
+						break;
+				}
+				break;
+
+				default:
+				{
+					output = "error:invalid call was made";
+					break;
+				}
+
+			}
+			// UDP
+			case 4:
 			{
 				if (length < 3){
 					output = "TCP calls requires at least 3 arguments. You tried: '" + input + "'";
@@ -168,9 +267,9 @@ void worker() {
 					}
 					else
 					{
-						output = newSocket(vect);
+						output = UDPNew(vect);
 					}
-				
+
 					break;
 				}
 				//send
@@ -181,9 +280,9 @@ void worker() {
 						break;
 					}
 					else{
-						output = TCPSend(vect);
+						output = UDPNew(vect);
 					}
-					
+
 
 					break;
 
@@ -196,10 +295,10 @@ void worker() {
 						output = "recieve requires at least 2 arguments. You tried: '" + input + "'";
 						break;
 					}
-					else { 
-						output = TCPRecv(vect); 
+					else {
+						output = UDPRecv(vect);
 					}
-					
+
 					break;
 				}
 
@@ -212,8 +311,8 @@ void worker() {
 						break;
 					}
 					else {
-						TCPSend(vect);
-						output = TCPRecv(vect);
+						UDPSend(vect);
+						output = UDPRecv(vect);
 					}
 					break;
 				}
@@ -229,25 +328,12 @@ void worker() {
 
 					break;
 				}
-
-				default:
-				{
-					break;
-				}
-
-				break;
-				}
-				break;
-
-			default:
-			{
-				output = "error:invalid call was made";
-				break;
-			}
-
-			}
 			
+				}
+				break;
+
 			}
+		}
 
 
 		//}
@@ -255,27 +341,27 @@ void worker() {
 			output = "an error occured unrecognized call was made please check the wiki for help. You tried: '" + input + "'";
 		}*/
 		//output = "unhandled call as of yet";
-			int outLength = output.length();
-			int bodyLength = maxLen - A2S_HEADER_SIZE;
-			if (outLength > maxLen){
-				ticket.PacketCount = 1 + ((outLength - 1) / bodyLength);
-				ticket.isLong = true;
+		int outLength = output.length();
+		int bodyLength = maxLen - A2S_HEADER_SIZE;
+		if (outLength > maxLen){
+			ticket.PacketCount = 1 + ((outLength - 1) / bodyLength);
+			ticket.isLong = true;
 
-				for (int x = 0; x < ticket.PacketCount; x ++){
+			for (int x = 0; x < ticket.PacketCount; x ++){
 
-					ticket.resultsArray.push_back(output.substr(x*bodyLength, bodyLength));
-
-				}
+				ticket.resultsArray.push_back(output.substr(x*bodyLength, bodyLength));
 
 			}
-			else
-			{
-				ticket.PacketCount = 1;
-			}
-		ticket.result = output; // prepare result 
-		ticket.ready = true; // notify about result 
-		
-		mtx.lock(); 
+
+		}
+		else
+		{
+			ticket.PacketCount = 1;
+		}
+		ticket.result = output; // prepare result
+		ticket.ready = true; // notify about result
+
+		mtx.lock();
 		tickets[cur_id] = ticket; // copy back the result
 		mtx.unlock();
 	}
@@ -310,27 +396,27 @@ int loadOptions(std::string name, std::string* address, int* timeout, int* proto
 	typedef std::vector< std::string > plugin_names_t;
 	plugin_names_t plugin_names;
 	std::string settings_type;
-	
+
 	//std::cout << protocolCStr << "\n";
 	// Setup options.
 	po::options_description desc("Options");
 	desc.add_options()
-		(timeoutCStr,
-		po::value<int >(timeout),
-		"Socket timeout (ms)")
+			(timeoutCStr,
+			 po::value<int >(timeout),
+			 "Socket timeout (ms)")
 
-		(addressCStr,
-		po::value< std::string >(address),
-		"Socket address (uri)")
+			(addressCStr,
+			 po::value< std::string >(address),
+			 "Socket address (uri)")
 
-		/*(portCStr,
-		po::value< std::string >(&port),
-		"Socket port")
-*/
-		(protocolCStr,
-		po::value< int >(protocol),
-		"Socket protocol (1:TCP,2:UDP)")
-		;
+			/*(portCStr,
+            po::value< std::string >(&port),
+            "Socket port")
+    */
+			(protocolCStr,
+			 po::value< int >(protocol),
+			 "Socket protocol (1:TCP,2:UDP)")
+			;
 	// Load setting file.
 	po::variables_map vm;
 	std::ifstream settings_file("a32s.ini", std::ifstream::in);
@@ -372,7 +458,7 @@ std::vector<std::string> split(const std::string &s, char delim) {
 };
 
 //RVExtension: Function called by arma, contains core threading framework,
-//Parameters: 
+//Parameters:
 //			char *output: pointer to output character array
 //			int outputSize: maximum output length
 //			const char *function: string input from arma
@@ -398,61 +484,61 @@ void __stdcall RVExtension(char *output, int outputSize, const char *function)
 		return;
 
 	}
-	
 
-	if (!strncmp(function, "r:", 2)) // detect checking for result 
+
+	if (!strncmp(function, "r:", 2)) // detect checking for result
 	{
-		long int num = atol(&function[2]); // ticket number or 0 
-		if (tickets.find(num) != tickets.end()) // ticket exists 
+		long int num = atol(&function[2]); // ticket number or 0
+		if (tickets.find(num) != tickets.end()) // ticket exists
 		{
-			mtx.lock(); 
+			mtx.lock();
 			if (tickets[num].ready) // result is ready
 			{
 				std::string header;
 				if (!tickets[num].isLong){
 					header = "0:";
 					std::string rtn = header + tickets[num].result;
-					strncpy_s(output, outputSize, (rtn.c_str()), _TRUNCATE);// result 
-					tickets.erase(num); // get rid of the read ticket 
-					
+					strncpy_s(output, outputSize, (rtn.c_str()), _TRUNCATE);// result
+					tickets.erase(num); // get rid of the read ticket
+
 				}
 				else
 				{
 					header = "1:" +
-						std::to_string(tickets[num].PacketCount) + A2S_DELIMINATOR +
-						std::to_string(tickets[num].lastSent + 2) + A2S_DELIMINATOR;
+							 std::to_string(tickets[num].PacketCount) + A2S_DELIMINATOR +
+							 std::to_string(tickets[num].lastSent + 2) + A2S_DELIMINATOR;
 					std::string rtn = header + tickets[num].resultsArray[tickets[num].lastSent + 1].c_str();
-						strncpy_s(output, outputSize, rtn.c_str(), _TRUNCATE);// result		
+					strncpy_s(output, outputSize, rtn.c_str(), _TRUNCATE);// result
 					tickets[num].lastSent++;
 					if (tickets[num].lastSent >= (tickets[num].PacketCount-1)){
-						tickets.erase(num); // get rid of the read ticket 
-						
+						tickets.erase(num); // get rid of the read ticket
+
 					}
 				}
 				mtx.unlock();
 				return;
 			}
 			mtx.unlock();
-			strncpy_s(output, outputSize, "WAIT", _TRUNCATE);    // result is not ready 
+			strncpy_s(output, outputSize, "WAIT", _TRUNCATE);    // result is not ready
 			return;
 		}
-		strncpy_s(output, outputSize, "EMPTY", _TRUNCATE); // no such ticket 
+		strncpy_s(output, outputSize, "EMPTY", _TRUNCATE); // no such ticket
 	}
-	else if (!strncmp(function, "s:", 2)) // detect ticket submission 
+	else if (!strncmp(function, "s:", 2)) // detect ticket submission
 	{
-		Data data; data.params = string(&function[2]); // extract params 
-		mtx.lock(); tickets.insert(pair<long int, Data>(++id, data)); // add ticket to the queue 
-		mtx.unlock(); if (!worker_working) // if worker thread is finished, start another one 
+		Data data; data.params = string(&function[2]); // extract params
+		mtx.lock(); tickets.insert(pair<long int, Data>(++id, data)); // add ticket to the queue
+		mtx.unlock(); if (!worker_working) // if worker thread is finished, start another one
 		{
-			worker_working = true; 
+			worker_working = true;
 			std::thread worker(worker);
-			worker.detach(); // start parallel process 
+			worker.detach(); // start parallel process
 		}
-		strncpy_s(output, outputSize, std::to_string(id).c_str(), _TRUNCATE); // ticket number 
+		strncpy_s(output, outputSize, std::to_string(id).c_str(), _TRUNCATE); // ticket number
 	}
 	else
 	{
-		strncpy_s(output, outputSize, "INVALID COMMAND", _TRUNCATE); // other input 
+		strncpy_s(output, outputSize, "INVALID COMMAND", _TRUNCATE); // other input
 	}
 }
 
@@ -509,7 +595,7 @@ std::string newSocket(std::vector<std::string>& vect)
 std::string TCPSend(std::vector<std::string>& vect)
 {
 	std::string rtn;
-	
+
 	try{
 
 		Poco::FIFOBuffer sendData(BUFFERLEN, false);
@@ -535,7 +621,7 @@ std::string TCPSend(std::vector<std::string>& vect)
 std::string TCPRecv(std::vector<std::string>& vect)
 {
 	std::string rtn;
-	
+
 //	std::cout << "Recieving data" << std::endl;
 	//Recieve
 	if (mySockets.find(vect[2]) == mySockets.end()) {
@@ -546,7 +632,7 @@ std::string TCPRecv(std::vector<std::string>& vect)
 		try {
 
 			Poco::FIFOBuffer recvData(BUFFERLEN);
-			
+
 			int iResult = mySockets[vect[2]]->receiveBytes(recvData);
 			char result[A2S_PACKET_SIZE] = "";
 
@@ -561,9 +647,9 @@ std::string TCPRecv(std::vector<std::string>& vect)
 //			std::cout << e.displayText() << std::endl;
 
 		}
-		
-	
-		
+
+
+
 		catch (boost::program_options::validation_error &e)
 		{
 			rtn = e.what();
@@ -576,8 +662,8 @@ std::string TCPRecv(std::vector<std::string>& vect)
 			rtn = "Error: an unhandled error occured";
 		}
 
-		
-		}
+
+	}
 	return rtn;
 }
 
@@ -605,8 +691,8 @@ std::string TCPClose(std::vector<std::string>& vect)
 
 
 		}
-		
-	
+
+
 		catch (boost::program_options::validation_error &e)
 		{
 			rtn = e.what();
@@ -638,7 +724,7 @@ std::string UDPNew(std::vector<std::string>& vect)
 				//				std::cout << timeout;
 				SocketAddress address(addrStr);
 				//				std::cout << addrStr;
-				DatagramSocket* mySocket = new DatagramSocket(address);
+				DatagramSocket* mySocket = new DatagramSocket();
 				Poco::Timespan timeoutSpan(timeout);
 				mySocket->setReceiveTimeout(timeoutSpan);
 				scktsMtx.lock();
@@ -658,29 +744,34 @@ std::string UDPNew(std::vector<std::string>& vect)
 			rtn = "the socket specified already exists";
 		}
 	}
-		catch (Poco::Exception &e){
-			rtn = e.displayText();
-		}
-		catch (boost::program_options::validation_error &e)
-		{
-			rtn = e.what();
-		}
-		catch (...){
-
-			rtn = "Error: an unhandled error occured";
-		}
-		return rtn;
+	catch (Poco::Exception &e){
+		rtn = e.displayText();
 	}
+	catch (boost::program_options::validation_error &e)
+	{
+		rtn = e.what();
+	}
+	catch (...){
+
+		rtn = "Error: an unhandled error occured";
+	}
+	return rtn;
+}
 
 std::string UDPSend(std::vector<std::string>& vect)
 {
 	std::string rtn;
+	std::string addrStr = "";
+	int timeout;
+	int protocol;
 
+	int iResult = loadOptions(vect[2], &addrStr, &timeout, &protocol);
+	SocketAddress address(addrStr);
 	try{
 
 		Poco::FIFOBuffer sendData(BUFFERLEN, false);
 		sendData.copy(vect[3].c_str(), vect[3].size());
-		int iResult = mySockets[vect[2]]->sendBytes(sendData);
+		int iResult = myUDPSockets[vect[2]]->sendTo(vect[2].c_str(), A2S_PACKET_SIZE,address);
 		rtn = "data sent, sendBytes returned: " + std::to_string(iResult);
 	}
 	catch (Poco::Exception &e){
